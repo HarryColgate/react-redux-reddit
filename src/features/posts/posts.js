86c0532bc88e0abcba0aebafe24adf5ddb3c7cdc
HarryColgate <harry.k.colgate@gmail.com>
2023-05-12 17:28:05 +0100

    start styling content

--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -31,14 +31,24 @@ export default function Posts() {
         return(
             <div className="posts">
                 <h1>r/{selectedSubreddit}</h1>
+                {/* goes through the feed array to make each post */}
                 {feed.feed.map(post => (
                     <div className="post">
                         <div className="votes">
+                            <i className="fas fa-caret-up"></i>
+                                <span>{post.score}</span>
+                            <i className="fas fa-caret-down"></i>
                         </div>
                         <div className="media">
                         </div>
                         <div className="content">
+                            <p className="sub"><span>{post.subreddit_name_prefixed}</span></p>
+                            <p className="postInfo">submitted by <span>u/{post.author}</span></p>
                             <h2>{post.title}</h2>
+                            <div className="comments">
+                                <i className="fas fa-comment"></i>
+                                {post.num_comments}
+                            </div>
                         </div>
                     </div>
                 ))}
